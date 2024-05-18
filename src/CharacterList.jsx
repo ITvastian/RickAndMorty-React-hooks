5e99781012be7350e8eece9a5bc443ab6669ee97
ITvastian <sebiquesada11@gmail.com>
2024-05-18 13:28:08 -0300

    buscardo

--- src/CharacterList.jsx
+++ src/CharacterList.jsx
@@ -17,8 +17,9 @@ function NavPage(props) {
 }
 
 function CharacterList() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
@@ -27,21 +28,40 @@ function CharacterList() {
       .catch((error) => console.error("Error fetching data:", error));
   }, [page]);
 
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const filteredData = data.filter((character) =>
+    character.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="container">
-      <NavPage page={page} setPage={setPage} />
-      <div className="row">
-        {data ? (
-          data.map((character) => (
+    <h1>Rick and Morty Characters</h1>
+    <input
+      type="text"
+      placeholder="Search characters"
+      value={searchTerm}
+      onChange={handleSearchChange}
+      className="form-control mb-4"
+    />
+    <NavPage page={page} setPage={setPage} />
+    <div className="row">
+      {data.length > 0 ? (
+        filteredData.length > 0 ? (
+          filteredData.map((character) => (
             <div className="col-md-4" key={character.id}>
               <Character character={character} />
             </div>
           ))
         ) : (
-          <p>Loading...</p>
-        )}
-      </div>
+          <p>No characters found</p>
+        )
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
+  </div>
   );
 }
 
